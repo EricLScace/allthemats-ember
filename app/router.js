d20a7c260f56b2f81eb3618f75e4ebd510c9b4e0
EricLScace <mail@ericscace.com>
2017-07-18 16:32:50 -0400

    Import auth files
    Why: clone & adapt auth process
    
    What: had to install ember-local-storage
    
    Test results: on page load
      Uncaught Error: Could not find module `ember-local-storage`
      imported from `allthemats/services/auth`
    
    Editorials: n/a
    
    Consequences: nothing loads; debugger halts
    
    Reference #42 clone auth process from Ember-auth

--- app/router.js
+++ app/router.js
@@ -1,12 +1,16 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
-});
+})
 
 Router.map(function() {
-});
+  this.route('sign-up')
+  this.route('sign-in')
+  this.route('change-password')
+  this.route('users')
+})
 
-export default Router;
+export default Router
