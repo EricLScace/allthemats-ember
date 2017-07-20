421f2dac72700a873bf839656faf4fd41b9c592f
EricLScace <mail@ericscace.com>
2017-07-20 17:12:48 -0400

    Copy code into sign-in-form files
    Test: neither form nor the <h4> from sign-in.hbs materialize
      on click, although URL does load.
    
    Issue #69

--- app/components/sign-in-form.js
+++ app/components/sign-in-form.js
@@ -1,4 +1,16 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Component.extend({
-});
+  tagName: 'form',
+  classNames: ['form-horizontal'],
+
+  actions: {
+    submit () {
+      this.sendAction('submit', this.get('credentials'))
+    },
+
+    reset () {
+      this.set('credentials', {})
+    }
+  }
+})
